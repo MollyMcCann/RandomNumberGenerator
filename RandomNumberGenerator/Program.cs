c762d9db55f7f48ea00e68c33b16edb03078ff7c
Molly Mccann <mollymccann@bellevuecollege.edu>
2021-01-30 11:26:05 -0800

    Add project files.

---
+++ RandomNumberGenerator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RandomNumberGenerator
+{
+    class Program
+    {
+        //ultimate goal is to write a caller for bingo we are starting with a random number generation and 
+        //will build from this point onward.
+        static void Main(string[] args)
+        {
+            //declaring number
+            int randomNumber;
+
+            Random randm = new Random();
+            //setting range of numbers we can choose from
+            randomNumber = randm.Next(1,91);
+            //printing our number
+            Console.WriteLine(" Your random number is " + randomNumber);
+            Console.ReadLine();
+
+        }
+    }
+}
